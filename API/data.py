b1a95c72f455f0697fa7354488ff724ac5005b61
pono33 <ponohuk@gmail.com>
2023-12-23 20:46:26 +0000

    data.py

--- API/data.py
+++ API/data.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 # Load DataFrames from Parquet files from the datasets folder
-playtime_genre = pd.read_parquet('datasets_API/playtime_genre.parquet')
-users_reviews = pd.read_parquet('datasets_API/users_reviews.parquet')
-
+playtime_genre = pd.read_parquet('datasets_API/playtime_genre.parquet', engine='pyarrow')
+users_reviews = pd.read_parquet('datasets_API/users_reviews.parquet', engine='pyarrow')
