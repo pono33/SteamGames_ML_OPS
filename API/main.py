6ab2da14cd1443d1b4f8190a7a7c87bbec30c692
pono33 <ponohuk@gmail.com>
2023-12-28 12:36:09 +0000

    API

--- API/main.py
+++ API/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.models import OAuthFlowAuthorizationCode
 
 # Import your functions from functions.py
 from functions import PlayTimeGenre, UserForGenre, UsersRecommend, UsersWorstDeveloper, sentiment_analysis, game_recommendations
 
 app = FastAPI()
 
-# Serve static files 
+# Serve static files
 app.mount("/", StaticFiles(directory="", html=True), name="static")
 
 # Index
@@ -19,6 +21,10 @@ async def index():
         <head>
             <title>Steam Query API</title>
             <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
+            <!-- Swagger UI -->
+            <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@3.49.0/swagger-ui.css" />
+            <!-- ReDoc -->
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/redoc@next/dist/redoc.min.css" />
             <style>
                 body {
                     font-family: Arial, sans-serif;
@@ -34,17 +40,81 @@ async def index():
                     font-size: 18px;
                     margin-top: 20px;
                 }
+                #swagger-ui {
+                    display: none;
+                }
+                #redoc {
+                    display: none;
+                }
+                #toggle-buttons {
+                    text-align: center;
+                    margin-top: 20px;
+                }
+                #toggle-buttons button {
+                    margin: 5px;
+                    padding: 10px;
+                    cursor: pointer;
+                }
             </style>
         </head>
         <body>
             <h1>Steam Platform Game Query API</h1>
-            <p>This tool that can be used by game developers to improve their games, by game publishers to track their sales, and by gamers to find new games to play.<p>
+            <p>This tool can be used by game developers to improve their games, by game publishers to track their sales, and by gamers to find new games to play.</p>
+            <div id="toggle-buttons">
+                <button onclick="showSwaggerUI()">Swagger UI</button>
+                <button onclick="showReDoc()">ReDoc</button>
+            </div>
+            <!-- Swagger UI container -->
+            <div id="swagger-ui"></div>
+            <!-- ReDoc container -->
+            <div id="redoc"></div>
+
+            <script src="https://unpkg.com/swagger-ui-dist@3.49.0/swagger-ui-bundle.js"></script>
+            <script src="https://unpkg.com/swagger-ui-dist@3.49.0/swagger-ui-standalone-preset.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/redoc@next/dist/redoc.min.js"></script>
+            <script>
+                function showSwaggerUI() {
+                    document.getElementById("swagger-ui").style.display = "block";
+                    document.getElementById("redoc").style.display = "none";
+                    initSwaggerUI();
+                }
+
+                function showReDoc() {
+                    document.getElementById("swagger-ui").style.display = "none";
+                    document.getElementById("redoc").style.display = "block";
+                    initReDoc();
+                }
+
+                function initSwaggerUI() {
+                    const ui = SwaggerUIBundle({
+                        url: "/docs/openapi.json",
+                        dom_id: '#swagger-ui',
+                        presets: [
+                            SwaggerUIBundle.presets.apis,
+                            SwaggerUIStandalonePreset
+                        ],
+                        layout: "BaseLayout"
+                    });
+                }
+
+                function initReDoc() {
+                    const ui = Redoc({
+                        specUrl: "/docs/openapi.json",
+                        dom_id: '#redoc',
+                        pathInMiddlePanel: true,
+                    });
+                }
+
+                // Show Swagger UI by default
+                showSwaggerUI();
+            </script>
         </body>
     </html>
     """
     return HTMLResponse(content=template)
 
-# API endpoints
+
+# API endpoints:
 
 @app.get("/playtime_genre/{genre}")
 async def read_playtime_genre(genre: str):
